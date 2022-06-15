7a06d103e2ed8167641c34e8c41cb3a1fdddb9e7
DianReyna <reynaarmas@gmail.com>
2022-06-15 16:25:07 -0500

    app - index

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import Allpokemons from "./components/AllPokemons/AllPokemons";
+import SearchBar from "./components/SearchBar/SearchBar";
+import { Route } from "react-router";
+import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import Types from "./components/Filters/Types";
+import DetailContainer from "./components/DetailContainer/DetailContainer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Route exact path="/" component={LandingPage} />
+
+      <Route path="/home">
+        <h2> Pokemons</h2>
+        <Types />
+        <SearchBar />
+        <Allpokemons />
+      </Route>
+
+      <Route path="/detail/:id" component={DetailContainer} />
     </div>
   );
 }
