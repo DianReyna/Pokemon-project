6b1ac2248ba6b5337bb3f609138ea108950d081c
DianReyna <reynaarmas@gmail.com>
2022-06-17 22:02:25 -0500

    component DetailConteiner

--- client/src/components/DetailContainer/DetailContainer.jsx
+++ client/src/components/DetailContainer/DetailContainer.jsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getPokemonsById } from "../../redux/actions";
+import { getPokemonsById, resetDetail } from "../../redux/actions";
 import PokemonDetail from "../PokemonDetail/PokemonDetail";
 
 export default function DetailContainer() {
-  const pokemon = useSelector((state) => state.pokemon);
+  const pokemon = useSelector((state) => state.detail);
   let { id } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPokemonsById(id));
+    dispatch(resetDetail());
   }, [dispatch, id]);
 
   return (
