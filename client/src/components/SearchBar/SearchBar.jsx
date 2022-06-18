d2838ca5231311cee59b6005075b098813f45e2d
DianReyna <reynaarmas@gmail.com>
2022-06-17 22:04:52 -0500

    component serchBar terminado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -6,20 +6,27 @@ export default function SearchBar() {
   const [search, setSerch] = useState("");
   const dispatch = useDispatch();
 
-  function handleOnSubmit(e) {
-    e.preventDefault();
-    dispatch(searchPokemon(search));
-  }
-  function inputChange(e) {
+  function handleInputChange(e) {
     setSerch(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(searchPokemon(search.toLocaleLowerCase()));
+    setSerch("");
   }
 
   return (
     <div>
-      <form onSubmit={handleOnSubmit}>
-        <input type="text" onChange={inputChange} value={search} />
-        <input type="submit" value="Buscar" />
-      </form>
+      <input
+        value={search}
+        onChange={(e) => handleInputChange(e)}
+        type="text"
+        placeholder="Pokemon name"
+      />
+      <button onClick={(e) => handleSubmit(e)} type="submit">
+        Buscar
+      </button>
     </div>
   );
 }
